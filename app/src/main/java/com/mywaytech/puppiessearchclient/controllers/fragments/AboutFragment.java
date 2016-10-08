efbb5c48462761781cb68658f3da0d1834478263
nemesis1346 <marco.maigua1346@gmail.com>
2016-10-08 16:00:34 -0500

    images integration intents 2

--- app/src/main/java/com/mywaytech/puppiessearchclient/controllers/fragments/AboutFragment.java
+++ app/src/main/java/com/mywaytech/puppiessearchclient/controllers/fragments/AboutFragment.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.mywaytech.puppiessearchclient.R;
 
@@ -14,6 +16,13 @@ import com.mywaytech.puppiessearchclient.R;
  * Created by nemesis1346 on 7/10/2016.
  */
 public class AboutFragment extends Fragment {
+
+    private TextView mCompanyName;
+    private TextView mCompanyEmail;
+    private TextView mCompanyPhone;
+    private ImageView mCompanyImage;
+    private TextView mCompanyLink;
+
     public static AboutFragment newInstance(){
         Bundle args = new Bundle();
         AboutFragment fragment = new AboutFragment();
@@ -31,6 +40,18 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_about,container, false);
+        mCompanyName = (TextView)rootView.findViewById(R.id.txt_company_name);
+        mCompanyImage = (ImageView) rootView.findViewById(R.id.image_company);
+        mCompanyPhone = (TextView) rootView.findViewById(R.id.txt_company_phone);
+        mCompanyEmail = (TextView) rootView.findViewById(R.id.txt_company_email);
+        mCompanyLink = (TextView) rootView.findViewById(R.id.txt_company_link);
+
+        mCompanyImage.setImageResource(R.mipmap.ic_company_logo);
+        mCompanyName.setText(getResources().getString(R.string.txt_company_name));
+        mCompanyEmail.setText(getResources().getString(R.string.txt_company_email));
+        mCompanyLink.setText(getResources().getString(R.string.txt_company_link));
+        mCompanyPhone.setText(getResources().getString(R.string.txt_company_phone));
+
         return rootView;
     }
 
