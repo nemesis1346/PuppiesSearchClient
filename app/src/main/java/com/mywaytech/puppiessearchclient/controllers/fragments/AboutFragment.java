bbb292e51294442fb0a9d1fde1f01afdb6d262e7
nemesis1346 <marco.maigua1346@gmail.com>
2016-10-10 11:09:52 -0500

    about fragment finished

--- app/src/main/java/com/mywaytech/puppiessearchclient/controllers/fragments/AboutFragment.java
+++ app/src/main/java/com/mywaytech/puppiessearchclient/controllers/fragments/AboutFragment.java
@@ -1,8 +1,14 @@
 package com.mywaytech.puppiessearchclient.controllers.fragments;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,6 +17,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.mywaytech.puppiessearchclient.R;
+import com.mywaytech.puppiessearchclient.utils.Utils;
 
 /**
  * Created by nemesis1346 on 7/10/2016.
@@ -23,7 +30,7 @@ public class AboutFragment extends Fragment {
     private ImageView mCompanyImage;
     private TextView mCompanyLink;
 
-    public static AboutFragment newInstance(){
+    public static AboutFragment newInstance() {
         Bundle args = new Bundle();
         AboutFragment fragment = new AboutFragment();
         fragment.setArguments(args);
@@ -39,8 +46,14 @@ public class AboutFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_about,container, false);
-        mCompanyName = (TextView)rootView.findViewById(R.id.txt_company_name);
+        View rootView = inflater.inflate(R.layout.fragment_about, container, false);
+
+        Toolbar toolbar = (Toolbar) rootView.findViewById(R.id.main_toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.aboutTitle);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mCompanyName = (TextView) rootView.findViewById(R.id.txt_company_name);
         mCompanyImage = (ImageView) rootView.findViewById(R.id.image_company);
         mCompanyPhone = (TextView) rootView.findViewById(R.id.txt_company_phone);
         mCompanyEmail = (TextView) rootView.findViewById(R.id.txt_company_email);
@@ -51,6 +64,16 @@ public class AboutFragment extends Fragment {
         mCompanyEmail.setText(getResources().getString(R.string.txt_company_email));
         mCompanyLink.setText(getResources().getString(R.string.txt_company_link));
         mCompanyPhone.setText(getResources().getString(R.string.txt_company_phone));
+
+        mCompanyEmail.setPaintFlags(mCompanyEmail.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        mCompanyLink.setPaintFlags(mCompanyLink.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        mCompanyPhone.setPaintFlags(mCompanyPhone.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        mCompanyLink.setLinkTextColor(Color.BLUE);
+
+        mCompanyImage.setOnClickListener(mCompanyLinkListener);
+        mCompanyLink.setOnClickListener(mCompanyLinkListener);
+        mCompanyEmail.setOnClickListener(mCompanyEmailListener);
+        mCompanyPhone.setOnClickListener(mCompanyPhoneListener);
 
         return rootView;
     }
@@ -65,4 +88,25 @@ public class AboutFragment extends Fragment {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    private View.OnClickListener mCompanyEmailListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Utils.openEmail(v.getContext(),new String[]{mCompanyEmail.getText().toString()},null, null);
+        }
+    };
+
+    private View.OnClickListener mCompanyLinkListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Utils.openBrowser(v.getContext(),mCompanyLink.getText().toString());
+        }
+    };
+
+    private View.OnClickListener mCompanyPhoneListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Utils.openPhone(v.getContext(),mCompanyPhone.getText().toString());
+        }
+    };
 }
--- app/src/main/java/com/mywaytech/puppiessearchclient/utils/Utils.java
+++ app/src/main/java/com/mywaytech/puppiessearchclient/utils/Utils.java
@@ -2,6 +2,9 @@ package com.mywaytech.puppiessearchclient.utils;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 
 import com.mywaytech.puppiessearchclient.R;
 
@@ -9,6 +12,8 @@ import com.mywaytech.puppiessearchclient.R;
  * Created by Marco on 28/9/2016.
  */
 public class Utils {
+
+
     public static void openEmail(Context context, String[] emails, String subject, String message) {
         //TODO: maybe check no email client
         Intent emailIntent = new Intent(Intent.ACTION_SEND);
@@ -18,5 +23,33 @@ public class Utils {
         emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, message == null ? "" : message);
         context.startActivity(Intent.createChooser(emailIntent, context.getString(R.string.select_email_title)));
     }
+
+    public static void openPhone(Context context, String phone) {
+        //TODO: maybe check no phone app
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phone));
+        context.startActivity(Intent.createChooser(phoneIntent, context.getString(R.string.select_phone_title)));
+    }
+
+    public static void openFacebook(Context context, String url) {
+        Intent facebookIntent = new Intent(Intent.ACTION_VIEW);
+        facebookIntent.setData(Uri.parse("fb://facewebmodal/f?href=" + url));
+
+        PackageManager packageManager = context.getPackageManager();
+        if (facebookIntent.resolveActivity(packageManager) != null) {
+            context.startActivity(facebookIntent);
+        } else {
+            openBrowser(context, url);
+        }
+    }
+
+    public static void openBrowser(Context context, @NonNull String url) {
+        //TODO: maybe check no browser
+        if (!url.startsWith("http://") && !url.startsWith("https://"))
+            url = "http://" + url;
+
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        context.startActivity(Intent.createChooser(browserIntent, context.getString(R.string.select_browser_title)));
+    }
+
 
 }
